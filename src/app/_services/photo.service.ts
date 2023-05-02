207cada58de717d407b279c6edae8f65fdf1db38
Krivobarac <gasparovicsasa@yahoo.com>
2023-05-03 00:42:42 +0200

    added posibility for taking and saving photo using camera

---
+++ src/app/_services/photo.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Preferences } from '@capacitor/preferences';
+import { UserPhoto } from '../_models/user-photo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  public photos: UserPhoto[] = [];
+  private PHOTO_STORAGE = 'photos';
+
+  constructor() {
+    this.loadSaved()
+  }
+
+  async addNewToGallery() {
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    })
+
+    const savedImageFile = await this.savePhoto(capturedPhoto);
+    this.photos.unshift(savedImageFile);
+
+    Preferences.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos)
+    })
+  }
+
+  private async savePhoto(photo: Photo): Promise<UserPhoto> {
+    const base64Data = await this.readAsBase64(photo);
+
+    const fileName = new Date().getTime() + '.jpeg'
+    await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    })
+    return {
+      filePath: fileName,
+      webViewPath: photo.webPath
+    };
+  }
+
+  private async readAsBase64(photo: Photo): Promise<string> {
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  private convertBlobToBase64(blob: Blob): Promise<unknown> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => resolve(reader.result);
+      reader.readAsDataURL(blob);
+    })
+  }
+
+  private async loadSaved() {
+    const value = (await Preferences.get({ key: this.PHOTO_STORAGE })).value;
+    this.photos = value ? JSON.parse(value) : [];
+
+    for (const photo of this.photos) {
+      const readFile = await Filesystem.readFile({
+        path: photo.filePath,
+        directory: Directory.Data
+      });
+
+      photo.webViewPath = `data:image/jpeg;base64,${readFile.data}`;
+    }
+  }
+}
